d8bcca6bb0d10fb7bf9498dc882c5e701c791895
Gustavo <alexandresg333@gmail.com>
2025-08-22 10:12:19 -0300

    comecei a fazer a lista 5

---
+++ roberto/lista5/main.c
@@ -0,0 +1,21 @@
+/*
+// questao 14
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  char *usr = "Hello";
+  const int size = strlen(usr);
+
+  int aux = 0;
+  for (int i = 0; i < size; i++) {
+    printf("\n");
+    for (int j = 0; j <= aux; j++) {
+      printf("%c", usr[j]);
+    }
+    aux++;
+  }
+  printf("\n");
+  return 0;
+}
+*/
